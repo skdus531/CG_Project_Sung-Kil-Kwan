46015fbce8e0641a8ae95148e84d1c36835654cc
skdus531 <50128050+skdus531@users.noreply.github.com>
2020-05-22 21:38:29 +0900

    첫 게임창 카메라 (eye, up 수정)

--- CG_TP/src/camera.h
+++ CG_TP/src/camera.h
@@ -3,9 +3,9 @@
 #define __CAMERA_H__
 struct camera
 {
-	vec3	eye = vec3(50, 0, 20);
+	vec3	eye = vec3(0.01f, 0, 30);
 	vec3	at = vec3(0, 0, 0);
-	vec3	up = vec3(-1, 0, 0);
+	vec3	up = vec3(0, 0, -1);
 	mat4	view_matrix = mat4::look_at(eye, at, up);
 
 	float	fovy = PI / 4.0f; // must be in radian
@@ -13,7 +13,11 @@ struct camera
 	float	dnear = 1.0f;
 	float	dfar = 1000.0f;
 	mat4	projection_matrix;
-};
 
+	void	update(){
+		eye = vec3(50, 0, 20);
+		up = vec3(-1, 0, 0);
+	}
+};
 
 #endif
