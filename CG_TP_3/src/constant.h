8230ca40716f01f5b568128c15b1a98482b6bc7b
skdus531 <50128050+skdus531@users.noreply.github.com>
2020-05-31 18:00:05 +0900

    Update constant.h

--- CG_TP_3/src/constant.h
+++ CG_TP_3/src/constant.h
@@ -1,17 +1,18 @@
 #pragma once
 struct game_state {
-	bool		game_start = false;
-	bool		title = true;
-	bool		how_to = false;
-	bool		option = false;
-	bool		game_over = false;
-	bool		game_clear = false;
-	bool		hid_game_clear = false;
-
+	bool		 game_start = false;
+	bool	  	 title = true;
+	bool	 	 how_to = false;
+	bool		 how_to_next = false;
+	bool		 option = false;
+	bool		 game_over = false;
+	bool		 game_clear = false;
+	bool		 hid_game_clear = false;
+	int			 no_assignment = 0;
+	int			 user_count = 1;
+	int			 user_life = 3;
+	int			 user_score = 0;
 	unsigned int game_mode = 0;
-	unsigned int user_count = 1;
-	int user_life = 3;
-	unsigned int user_score = 0;
 	unsigned int game_level = 0;
 	unsigned int game_stage = 1;
 };
@@ -22,6 +23,6 @@ vec3 subject_color[] = {
 	vec3(0.7f, 0.2f, 0.3f),
 	vec3(0.1f, 0.4f, 0.6f)
 };
-vec3 assignment_color = vec3(1.0f, 1.0f, 1.0f);
-vec3 lecture_color = vec3(0.01f, 0.01f, 0.01f);
+vec3 assignment_color = vec3(1.0f, 1.0f, 0.0f);
+vec3 lecture_color = vec3(1.0f, 1.0f, 1.0f);
 unsigned int subject_count = 3;
